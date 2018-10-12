f4a1bf6dc9516f0d4e33704b02ba8bab3f42110b
Michał Grzesik <mgrzesik7@gmail.com>
2018-10-12 04:27:50 +0200

    Added Multithreding downloading data from API

--- backend/downloader/src/main/java/pl/edu/agh/downloader/task/DataDownloader.java
+++ backend/downloader/src/main/java/pl/edu/agh/downloader/task/DataDownloader.java
@@ -8,6 +8,8 @@ import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 import pl.edu.agh.downloader.domain.db.entity.Measurement;
 import pl.edu.agh.downloader.domain.db.entity.Sensor;
@@ -18,6 +20,7 @@ import pl.edu.agh.downloader.domain.net.Link;
 import pl.edu.agh.downloader.domain.net.RowMeasurement;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -30,7 +33,7 @@ import java.util.Optional;
 @Component
 public class DataDownloader {
 
-    private static final String DATA_SERVER_ADDRESS = "http://webtris.highwaysengland.co.uk/api/v1/reports/01022016/to/31032016/daily?sites=%s&page=1&page_size=200";
+    private static final String DATA_SERVER_ADDRESS = "http://webtris.highwaysengland.co.uk/api/v1/reports/01012015/to/31122015/daily?sites=%s&page=1&page_size=2000";
 
     private final MultiThreadedHttpConnectionManager httpConnectionManager;
     private final ObjectMapper objectMapper;
@@ -49,30 +52,31 @@ public class DataDownloader {
     }
 
 
-
-    @PostConstruct
-    private void download() {
-        log.info("*Init task -  downloading data .");
+    @Async
+    public void download(int firstSensorId, int lastSensorId) {
+        log.info(String.format("*Init task -  downloading data for range: <%d, %d)  .", firstSensorId, lastSensorId));
 
         HttpClient client = new HttpClient(httpConnectionManager);
         String res;
         GetMethod get = new GetMethod(String.format(DATA_SERVER_ADDRESS, String.valueOf(1)));
-        try {
-            for (int id = 1; id < 18000; id++) {
+        for (int id = firstSensorId; id < lastSensorId; id++) {
+            try {
+
                 get = new GetMethod(String.format(DATA_SERVER_ADDRESS, String.valueOf(id)));
 
                 ApiResponse apiResponse;
                 String nextAddress;
-                log.info("*Downloading data from server has been started.");
+                log.info("*Downloading data from server has been started for sensor=." + id);
                 do {
                     int statusCode = client.executeMethod(get);
                     if (statusCode != HttpStatus.SC_OK) {
-                        log.error("Method failed: " + get.getStatusLine());
+                        log.error("Method failed: " + get.getStatusLine() + "for sensor_id: " + String.valueOf(id) + "with status: " + statusCode);
                         break;
+
                     } else {
                         res = get.getResponseBodyAsString();
                         apiResponse = objectMapper.readValue(res, ApiResponse.class);
-                        System.out.println(apiResponse);
+                        log.info(apiResponse.toString());
                         persistObjectToDb(apiResponse, id);
                     }
                     nextAddress = getNextAddress(apiResponse);
@@ -81,23 +85,25 @@ public class DataDownloader {
                     get = new GetMethod(nextAddress);
                 } while (nextAddress != null);
 
+
+            } catch (HttpException e) {
+                log.error("Fatal protocol violation: " + e.getMessage());
+                e.printStackTrace();
+            } catch (IOException e) {
+                log.error(e.getMessage());
+                e.printStackTrace();
+            } finally {
+                get.releaseConnection();
+
             }
 
-        } catch (HttpException e) {
-            log.error("Fatal protocol violation: " + e.getMessage());
-            e.printStackTrace();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-            e.printStackTrace();
-        } finally {
-            get.releaseConnection();
-            log.info("*Finished downloading data.");
-
         }
+        log.info(String.format("*Finished downloading data for range : <%d, %d)", firstSensorId, lastSensorId));
+
     }
 
     private String getNextAddress(ApiResponse apiResponse) {
-        for (Link link: apiResponse.getHeader().getLinks()) {
+        for (Link link : apiResponse.getHeader().getLinks()) {
             if (link.getRel().equals("nextPage")) {
                 return link.getHref();
             }
@@ -108,7 +114,7 @@ public class DataDownloader {
     private void persistObjectToDb(ApiResponse apiResponse, long id) {
         Optional<Sensor> sensorOpt;
         Sensor sensor;
-        if (apiResponse.getRows().size()>0) {
+        if (apiResponse.getRows().size() > 0) {
             sensorOpt = sensorRepository.findById(id);
 
 
@@ -118,16 +124,16 @@ public class DataDownloader {
             } else {
                 sensor = sensorOpt.get();
             }
-        }else{
+        } else {
             return;
         }
 
         List<Measurement> measurements = new ArrayList<>(apiResponse.getRows().size());
-        for (RowMeasurement row: apiResponse.getRows()) {
+        for (RowMeasurement row : apiResponse.getRows()) {
             Measurement measurement = Measurement.builder()
                     .avgMph(row.getAvgMph())
                     .dateTime(LocalDateTime.of(LocalDateTime.parse(row.getMeasurementDate(), dateTimeFormatter).toLocalDate(), LocalTime.parse(row.getTimeOfMeasurement(), timeFormatter)))
-                    .status(row.getAvgMph() != null && row.getAvgMph() > 0 ? "SUCCESSFUL":"NO_DATA")
+                    .status(row.getAvgMph() != null && row.getAvgMph() > 0 ? "SUCCESSFUL" : "NO_DATA")
                     .sensor(sensor)
                     .totalVolume(row.getTotalVolume())
                     .build();
