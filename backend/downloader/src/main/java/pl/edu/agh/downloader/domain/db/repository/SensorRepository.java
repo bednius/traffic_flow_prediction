0a58030f96e70fe9737e8fc7615372b42980ff65
Michał Grzesik <mgrzesik7@gmail.com>
2018-12-20 13:39:44 +0100

    cleaned repo

--- backend/downloader/src/main/java/pl/edu/agh/downloader/domain/db/repository/SensorRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.agh.downloader.domain.db.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.edu.agh.downloader.domain.db.entity.Sensor;
-
-public interface SensorRepository extends JpaRepository<Sensor, Long> {
-}
