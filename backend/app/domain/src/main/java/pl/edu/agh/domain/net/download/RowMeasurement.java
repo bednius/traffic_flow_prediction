14583684f2fa081a0602e33c927113e05680200a
Michał Grzesik <mgrzesik7@gmail.com>
2018-12-20 13:18:28 +0100

    Remove redundant imports

--- backend/app/domain/src/main/java/pl/edu/agh/domain/net/download/RowMeasurement.java
+++ backend/app/domain/src/main/java/pl/edu/agh/domain/net/download/RowMeasurement.java
@@ -1,16 +1,10 @@
 package pl.edu.agh.domain.net.download;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 
 @Builder
